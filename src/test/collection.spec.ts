266e3591b7e57d674b5718532bb9360a0c37acc3
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-12-07 03:04:15 +0000

    test: small spec.

---
+++ src/test/collection.spec.ts
@@ -0,0 +1,23 @@
+import { SetAdapter } from "../adapter";
+import { Collection } from "../lib";
+
+const collection = new Collection(SetAdapter, 1, 2, 3);
+
+// Adds.
+collection.add(27, 29, 31, 33);
+// Deletes.
+collection.delete(29, 31);
+
+console.log(`size: `, collection.size); // Output: 3
+
+describe("Collection SetAdapter", () => {
+  test("has method works correctly", () => {
+    expect(collection.has(27)).toBe(true);
+    expect(collection.has(29)).toBe(false);
+  });
+
+  test("clear method works correctly", () => {
+    collection.clear();
+    expect(collection.size).toBe(0);
+  });
+});
