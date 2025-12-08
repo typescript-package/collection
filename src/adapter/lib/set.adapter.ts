0d3e59c229654d092e238bad904ecef560f1cbe9
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-12-08 00:15:04 +0000

    refactor(SetAdapter): improve type definitions and simplify methods.

--- src/adapter/lib/set.adapter.ts
+++ src/adapter/lib/set.adapter.ts
@@ -4,72 +4,56 @@ import { CollectionAdapter } from '@typedly/collection';
  * @description The Set collection adapter.
  * @export
  * @class SetAdapter
- * @template Element The type of the elements in the Set.
- * @template [Type=Set<Element>] The type of the underlying Set collection.
- * @implements {CollectionAdapter<Element, Type>}
+ * @template E The type of the elements in the Set.
+ * @template [T=Set<E>] The type of the underlying Set collection.
+ * @implements {CollectionAdapter<E, T>}
  */
 export class SetAdapter<
-  Element,
-  Type = Set<Element>
-> implements CollectionAdapter<Element, Type> {
+  E,
+  T extends Set<E> = Set<E>
+> implements CollectionAdapter<E, T> {
   public version: string = '1.0.0';
-
   get [Symbol.toStringTag](): string {
     return 'SetAdapter';
   }
-
-  get [Symbol.iterator](): Iterator<Element> {
-    return (this.#collection as unknown as Set<Element>)[Symbol.iterator]();
+  [Symbol.iterator](): Iterator<E> {
+    return this.#collection[Symbol.iterator]();
   }
-
-
-  protected get collection(): Type {
+  protected get collection(): T {
     return this.#collection;
   }
-
   public get size(): number {
-    return (this.collection as unknown as Set<Element>).size;
+    return this.collection.size;
   }
-
-  public get value(): Type {
-    return this.collection as unknown as Type;
+  public get value(): T {
+    return this.collection as unknown as T;
   }
-
-  #collection: Type;
-
-  constructor(...collection: Element[]) {
-    this.#collection = new Set(collection) as Type;
+  #collection: T;
+  constructor(...collection: E[]) {
+    this.#collection = new Set(collection) as T;
   }
-
-  public add(element: Element): this {
-    return (this.collection as unknown as Set<Element>).add(element), this;
+  public add(...element: E[]): this {
+    return element.forEach(e => this.collection.add(e)), this;
   }
-
   public clear(): this {
-    return (this.collection as unknown as Set<Element>).clear(), this;
+    return this.collection.clear(), this;
   }
-
   public destroy(): this {
     return this.clear(), (this.#collection = null as any), this;
   }
-
-  public delete(element: Element): boolean {
-    return (this.collection as unknown as Set<Element>).delete(element);
+  public delete(...element: E[]): boolean {
+    return element.every(e => this.collection.delete(e));
   }
-
-  public forEach(callbackfn: (element: Element, element2: Element, collection: CollectionAdapter<Element, Type>) => void, thisArg?: any): this {
-    return (this.collection as unknown as Set<Element>).forEach((value, value2) => callbackfn.call(thisArg, value, value2, this as any)), this;
+  public forEach(callbackfn: (element: E, element2: E, collection: CollectionAdapter<E, T>) => void, thisArg?: any): this {
+    return this.collection.forEach((value, value2) => callbackfn.call(thisArg, value, value2, this as any)), this;
   }
-
-  public has(element: Element): boolean {
-    return (this.collection as unknown as Set<Element>).has(element);
+  public has(...element: E[]): boolean {
+    return element.every(e => this.collection.has(e));
   }
-
   public lock(): this {
     return Object.freeze(this.collection), this;
   }
-
-  public set(value: Type): this {
+  public set(value: T): this {
     return (this.#collection = value), this;
   }
 }
