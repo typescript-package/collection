c37de39803ca68cce592761c883639a909385e61
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-12-08 00:17:57 +0000

    refactor(`Collection`): change the type of `A` constrainment and in docs.

--- src/lib/collection.class.ts
+++ src/lib/collection.class.ts
@@ -7,12 +7,12 @@ import { CollectionAdapter } from "@typedly/collection";
  * @export
  * @class Collection
  * @template E The type of elements in the collection.
- * @template {new (...args: any[]) => CollectionAdapter<E, any>} A The type of the adapter.
+ * @template {new (...elements: E[]) => CollectionAdapter<E, any>} A The type of the adapter.
  * @extends {CollectionBase<E, any, InstanceType<A>>}
  */
 export class Collection<
   E,
-  A extends new (...args: any[]) => CollectionAdapter<E, any>
+  A extends new (...elements: E[]) => CollectionAdapter<E, any>
 > extends CollectionBase<E, any, InstanceType<A>> {
   constructor(adapter: A, ...elements: E[]) {
     super(adapter as any, ...elements);
