f915cffa55aa6e73ddbee899023ddeb6a083400b
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-12-21 03:28:07 +0000

    refactor(`Collection`): use optional param `value` to capture the `T` type properly, and make `async` optional.

--- src/lib/collection.class.ts
+++ src/lib/collection.class.ts
@@ -6,22 +6,26 @@ import { CollectionAdapter } from '@typedly/collection';
  * @description The collection concrete class with adapter support.
  * @export
  * @class Collection
- * @template E The type of elements in the collection.
- * @template {boolean} R The `boolean` type to determine async methods.
- * @template {new (...elements: E[]) => CollectionAdapter<E, any, R>} A The type of the adapter.
- * @extends {CollectionBase<E, A extends new (...args: E[]) => CollectionAdapter<E, infer T, R> ? T : never, R, InstanceType<A>>}
+ * @template T 
+ * @template [E=unknown] The type of elements in the collection.
+ * @template {boolean} [R=false] The `boolean` type to determine async methods.
+ * @template {CollectionAdapter<E, T, R>} [A=CollectionAdapter<E, T, R>] The type of the adapter.
+ * @extends {CollectionBase<E, T, R, A>}
  */
 export class Collection<
+  T,
   E,
-  R extends boolean,
-  A extends new (...elements: E[]) => CollectionAdapter<E, any, R>
-> extends CollectionBase<
-  E,
-  A extends new (...args: E[]) => CollectionAdapter<E, infer T, R> ? T : never,
-  R,
-  InstanceType<A>
-> {
-  constructor(async: R, adapter: A, ...elements: E[]) {
-    super(async, adapter as any, ...elements);
+  R extends boolean = false,
+  A extends CollectionAdapter<E, T, R> = CollectionAdapter<E, T, R>
+> extends CollectionBase<E, T, R, A> {
+  constructor(
+    {async, value}: {
+      async?: R,
+      value?: T,
+    },
+    adapter: {new (...elements: E[]): A},
+    ...elements: E[]
+  ) {
+    super(async || false as R, adapter, ...elements);
   }
 }
