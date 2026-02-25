ae0c9b2f733bdf6b84a556ad5f68a679a703cf53
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-02-25 20:15:07 +0000

    feat(`ConfigurableCollection`): add concrete class with adapter support and type templates

---
+++ src/lib/configurable.collection.ts
@@ -0,0 +1,35 @@
+// Abstract.
+import { ConfigurableCollectionBase } from '../core/lib';
+// Interface.
+import { CollectionAdapter, CollectionSettings, ConfigurableCollectionAdapterConstructor } from '@typedly/collection';
+/**
+ * @description The configurable collection concrete class with adapter support.
+ * @export
+ * @class ConfigurableCollection
+ * @template {CollectionAdapter<E, T, R>} A The type of the adapter.
+ * @template {CollectionSettings<E, T, any>} C The type of settings.
+ * @template [E=A extends CollectionAdapter<infer E, any, any> ? E : unknown] The type of elements in the collection, captured via adapter.
+ * @template [T=C['value'] extends undefined | unknown ? A extends CollectionAdapter<any, infer T, any> ? T : unknown : C['value']] The type of the collection's value captured via adapter.
+ * @template {boolean} [R=C['async'] extends boolean ? C['async'] : A extends CollectionAdapter<E, any, infer R> ? R : false] The `boolean` type to determine async methods.
+ * @extends {ConfigurableCollectionBase<E, T, R, C, A>}
+ */
+export class ConfigurableCollection<
+  A extends CollectionAdapter<E, T, R>,
+  const C extends CollectionSettings<E, T, any>,
+  E = A extends CollectionAdapter<infer E, any, any> ? E : unknown,
+  T = C['value'] extends undefined | unknown ? A extends CollectionAdapter<any, infer T, any> ? T : unknown : C['value'],
+  R extends boolean = C['async'] extends boolean ? C['async'] : A extends CollectionAdapter<E, any, infer R> ? R : false,
+> extends ConfigurableCollectionBase<E, T, R, C, A> {
+  override get [Symbol.toStringTag](): string {
+    return 'ConfigurableCollection';
+  }
+  constructor(
+    settings: C = {} as C,
+    adapter: ConfigurableCollectionAdapterConstructor<E, T, C, A>,
+    ...elements: E[]
+  ) {
+    super(settings, adapter, ...elements);
+  }
+
+
+}
