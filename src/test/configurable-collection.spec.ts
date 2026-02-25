1547edcf79fc6b7d003a64e400c39950e99a5807
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-02-25 20:16:24 +0000

    test: add

---
+++ src/test/configurable-collection.spec.ts
@@ -0,0 +1,31 @@
+import { ConfigurableCollection } from "../lib";
+import { SetAdapter } from '@typescript-package/collection-adapter';
+
+// const collection = new ConfigurableCollection(
+//   {async: false},
+//   SetAdapter,
+//   1, 2, '3' as string | number
+// );
+
+// // Adds.
+// collection.add(27, 29, 31);
+// // Deletes.
+// collection.delete(29, 31, 22);
+
+// for (const element of collection) {
+//   console.log(`element: `, element);
+// }
+
+// console.log(`size: `, collection.size); // Output: 4
+
+// describe("HybridCollection SetAdapter", () => {
+//   test("has method works correctly", () => {
+//     expect(collection.has(27)).toBe(true);
+//     expect(collection.has(29)).toBe(false);
+//   });
+
+//   test("clear method works correctly", () => {
+//     collection.clear();
+//     expect(collection.size).toBe(0);
+//   });
+// });
